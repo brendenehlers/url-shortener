68b4d7a07a326e420a68c677bdf79a89500ee495
Brenden Ehlers <brenden@theehlers.org>
2025-08-23 21:02:21 -0600

    partial: implement short url algorithm
    format

--- shortener-service/src/main/kotlin/com/behlers/shortener/service/url/service/UrlService.kt
+++ shortener-service/src/main/kotlin/com/behlers/shortener/service/url/service/UrlService.kt
@@ -3,10 +3,15 @@ package com.behlers.shortener.service.url.service
 import com.behlers.shortener.service.url.domain.Url
 import com.behlers.shortener.service.url.domain.UrlNotFoundException
 import com.behlers.shortener.service.url.repository.UrlRepository
+import java.security.MessageDigest
 import org.springframework.stereotype.Service
 
 @Service
 class UrlService(private val urlRepository: UrlRepository) {
+
+  companion object {
+    val md5 = MessageDigest.getInstance("MD5")
+  }
 
   // TODO replace this with actual algorithm
   private var tempCode = "1"
@@ -30,8 +35,29 @@ class UrlService(private val urlRepository: UrlRepository) {
   }
 
   private fun createShortCode(longUrl: String): String {
-    val newCode = tempCode
-    tempCode = (tempCode.toInt() + 1).toString()
-    return newCode
+    val digest = md5.digest(longUrl.toByteArray())
+    return encodeToBase62(byteArrayToLong(digest))
+  }
+
+  private fun byteArrayToLong(arr: ByteArray): Long {
+    TODO("TODO implement this")
+  }
+
+  private fun encodeToBase62(number: Long): String {
+    val alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val base = alphabet.length
+    var num = number
+    val encoded = StringBuilder()
+
+    if (num == 0L) {
+      return alphabet[0].toString() // Handle zero case
+    }
+
+    while (num > 0) {
+      val remainder = (num % base).toInt()
+      encoded.insert(0, alphabet[remainder])
+      num /= base
+    }
+    return encoded.toString()
   }
 }
