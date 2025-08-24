109abbf4ba6eb3aefd916fce020beb62675be2a9
Brenden Ehlers <brenden@theehlers.org>
2025-08-23 19:04:50 -0600

    test crud api and create postman collection

--- shortener-service/src/main/kotlin/com/behlers/shortener/service/url/service/UrlService.kt
+++ shortener-service/src/main/kotlin/com/behlers/shortener/service/url/service/UrlService.kt
@@ -2,25 +2,28 @@ package com.behlers.shortener.service.url.service
 
 import com.behlers.shortener.service.url.domain.Url
 import com.behlers.shortener.service.url.repository.UrlRepository
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
 @Service
 class UrlService(
-  val urlRepository: UrlRepository
+  val urlRepository: UrlRepository,
+  val r2dbcEntityTemplate: R2dbcEntityTemplate,
 ) {
+
+  // TODO replace this with actual algorithm
+  private var tempCode = "1"
 
   fun getUrl(shortCode: String): Mono<Url> {
     return urlRepository.getUrlByShortCode(shortCode)
   }
 
   fun createUrl(longUrl: String): Mono<Url> {
-    return urlRepository.save(
-      Url(
+    return r2dbcEntityTemplate.insert(Url(
         createShortCode(longUrl),
         longUrl,
-    )
-    )
+    ))
   }
 
   fun updateUrl(shortCode: String, longUrl: String): Mono<Url> {
@@ -35,6 +38,8 @@ class UrlService(
   }
 
   private fun createShortCode(longUrl: String): String {
-    TODO("Not implemented")
+    val newCode = tempCode
+    tempCode = (tempCode.toInt() + 1).toString()
+    return newCode
   }
 }
