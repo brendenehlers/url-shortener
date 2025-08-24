a6c4a0698b42822c0d1c01496eb57a425af1a54a
Brenden Ehlers <brenden@theehlers.org>
2025-08-23 20:05:50 -0600

    make database operations transactional

--- shortener-service/src/main/kotlin/com/behlers/shortener/service/url/repository/UrlRepository.kt
+++ shortener-service/src/main/kotlin/com/behlers/shortener/service/url/repository/UrlRepository.kt
@@ -1,8 +1,10 @@
 package com.behlers.shortener.service.url.repository
 
 import com.behlers.shortener.service.url.domain.Url
+import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
 
+@Transactional
 interface UrlRepository : JpaRepository<Url, String> {
   fun getUrlByShortCode(shortCode: String): Url?
 
