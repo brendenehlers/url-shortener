7a34fdfebd18befc242bf56deb0f8d0ca7e488b4
Brenden Ehlers <brenden@theehlers.org>
2025-08-24 19:33:19 -0600

    happy path validations on crud endpoints

--- src/test/kotlin/com/behlers/shortener/service/url/controller/UrlCrudControllerTest.kt
+++ src/test/kotlin/com/behlers/shortener/service/url/controller/UrlCrudControllerTest.kt
@@ -2,10 +2,13 @@ package com.behlers.shortener.service.url.controller
 
 import com.behlers.shortener.service.TestContainerBase
 import com.behlers.shortener.service.url.domain.CreateUrlRequestBody
+import com.behlers.shortener.service.url.domain.DeleteUrlResponseBody
+import com.behlers.shortener.service.url.domain.UpdateUrlRequestBody
 import com.behlers.shortener.service.url.domain.UrlEntity
 import com.behlers.shortener.service.url.repository.UrlRepository
 import io.kotest.matchers.date.shouldBeAfter
 import io.kotest.matchers.date.shouldBeBefore
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.AfterEach
@@ -53,9 +56,8 @@ class UrlCrudControllerTest : TestContainerBase() {
   fun `gets url from database after calling getUrl`() {
     val longUrl = "https://test.com"
     val shortCode = "abcdefgh"
-    val now = Instant.now()
 
-    urlRepository.save(UrlEntity(shortCode, longUrl, now, now))
+    urlRepository.save(UrlEntity(shortCode, longUrl))
 
     webClient
       .get()
@@ -65,10 +67,61 @@ class UrlCrudControllerTest : TestContainerBase() {
       .value {
         it.shortCode shouldBe shortCode
         it.longUrl shouldBe longUrl
+      }
+  }
+
+  @Test
+  fun `timestamps are saved to db`() {
+    val shortCode = "abcdefgh"
+    val now = Instant.now()
+
+    urlRepository.save(UrlEntity(shortCode, "https://test.com", now, now))
+
+    webClient
+      .get()
+      .uri("/api/v1/url/$shortCode")
+      .exchange()
+      .expectBody(UrlEntity::class.java)
+      .value {
         it.createdAt shouldBeBefore now.plusSeconds(1)
         it.createdAt shouldBeAfter now.minusSeconds(1)
         it.updatedAt shouldBeBefore now.plusSeconds(1)
         it.updatedAt shouldBeAfter now.minusSeconds(1)
       }
+  }
+
+  @Test
+  fun `updates url in the database after calling updateUrl`() {
+    val shortCode = "abcdefgh"
+    urlRepository.save(UrlEntity(shortCode, "https://old.test.com"))
+
+    val newLongUrl = "https://new.test.com"
+    webClient
+      .post()
+      .uri("/api/v1/url/$shortCode")
+      .bodyValue(UpdateUrlRequestBody(newLongUrl))
+      .exchange()
+      .expectBody(UrlEntity::class.java)
+      .value {
+        it.shortCode shouldBe shortCode
+        it.longUrl shouldBe newLongUrl
+      }
+  }
+
+  @Test
+  fun `deletes url after calling deleteUrl`() {
+    val shortCode = "abcdefgh"
+    urlRepository.save(UrlEntity(shortCode, "https://test.com"))
+
+    webClient
+      .delete()
+      .uri("/api/v1/url/$shortCode")
+      .exchange()
+      .expectBody(DeleteUrlResponseBody::class.java)
+      .value {
+        it.message shouldBe "success"
+      }
+
+    urlRepository.getUrlByShortCode(shortCode).shouldBeNull()
   }
 }
